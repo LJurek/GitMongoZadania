d5384b75f433c54e670ea6ea72bfa738a9384d18
Lukasz <lukasz.jurek86@gmail.com>
2018-04-22 14:25:29 +0200

    first commit

---
+++ findNested.js
@@ -0,0 +1,34 @@
+// db = connect("localhost:27017/files");
+use films
+
+db.film.find({"year":{$gt:2016}}).pretty()
+
+//db.film.insert(
+//{
+//	"title": "Marsjanin", 
+//	"year": 2014, 
+//	"director": {
+//		"firstname": "Ridley",
+//		"lastname": "Scott",
+//		"birthday": "1976-05-02"
+//	}
+//}
+//)
+
+db.film.aggregate(
+[
+{
+	$group:{
+		_id:'$year',
+		total: {sum:1}
+	}
+}
+]
+)
+
+db.film.find({"director.lastname": "Scott"}).pretty()
+
+cursor = db.film.find()
+while (cursor.hasNext() ) {
+	printjson(cursor.next() );
+}
